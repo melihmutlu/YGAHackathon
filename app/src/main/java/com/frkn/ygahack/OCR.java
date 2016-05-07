07ac64408ee21f41fc771b0966a74756131f4090
Erkam Seker <serkamseker@gmail.com>
2016-05-08 00:21:00 +0300

    ocr added.

---
+++ app/src/main/java/com/frkn/ygahack/OCR.java
@@ -0,0 +1,81 @@
+package com.frkn.ygahack;
+
+import org.json.JSONObject;
+
+import java.io.File;
+
+import cz.msebera.android.httpclient.client.HttpClient;
+import cz.msebera.android.httpclient.client.methods.HttpPost;
+import cz.msebera.android.httpclient.entity.ContentType;
+import cz.msebera.android.httpclient.entity.mime.HttpMultipartMode;
+import cz.msebera.android.httpclient.entity.mime.MultipartEntityBuilder;
+import cz.msebera.android.httpclient.entity.mime.content.FileBody;
+import cz.msebera.android.httpclient.impl.client.DefaultHttpClient;
+import cz.msebera.android.httpclient.util.EntityUtils;
+
+/**
+ * Created by erkam on 8.05.2016.
+ */
+public class OCR {
+    private static OCR ocr;
+    private File file;
+
+    //Returns cloudsight object
+    public static OCR getInstance(){
+        if(ocr==null)
+            return new OCR();
+        else
+            return ocr;
+    }
+
+    public JSONObject postImage(File file){
+        this.file = file;
+        JSONObject object=null;
+        // Create a new HttpClient and Post Header
+        HttpClient httpclient = new DefaultHttpClient();
+        HttpPost httppost = new HttpPost("https://api.ocr.space/parse/image");
+        //httppost.addHeader("apikey", "1007e8976188957");
+        //httppost.addHeader("language", "tur");
+        //httppost.addHeader("isOverlayRequired", "false");
+
+        JSONObject json=null;
+
+        try {
+            MultipartEntityBuilder builder = MultipartEntityBuilder.create();
+
+            builder.setMode(HttpMultipartMode.BROWSER_COMPATIBLE);
+            FileBody fileBody = new FileBody(file,"image/jpeg"); //image should be a String
+            builder.addBinaryBody("file", file, ContentType.MULTIPART_FORM_DATA, file.getName());
+            builder.addTextBody("apikey", "1007e8976188957");
+            builder.addTextBody("language","tur");
+            builder.addTextBody("isOverlayRequired","false");
+            httppost.setEntity(builder.build());
+
+           /* // Add your data
+            List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(2);
+            nameValuePairs.add(new BasicNameValuePair("image_request[remote_image_url]", "12345"));
+            nameValuePairs.add(new BasicNameValuePair("image_request[language]", "tr-TR"));
+            nameValuePairs.add(new BasicNameValuePair("image_request[locale]", "tr-TR"));
+            httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+*/
+
+            String responseBody;
+
+            try {
+                responseBody = EntityUtils.toString(httpclient.execute(httppost).getEntity(), "UTF-8");
+
+                object = new JSONObject(responseBody);
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                httpclient.getConnectionManager().shutdown();
+            }
+
+
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+        }
+        return object;
+    }
+}
